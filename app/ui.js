66665f18acad3c08a80567e303abf7889679cccf
gundam1993 <gundam1993@gmail.com>
2017-07-10 13:08:05 +0800

    rebuild buildGameUI function in ui module

--- app/ui.js
+++ app/ui.js
@@ -9,7 +9,7 @@ const UiUploader = function () {
 
 UiUploader.prototype.init = function() {
   EE.on('joinFail', this.showHint)
-  EE.on('joinSuccess', this.enterGameUI)
+  EE.on('joinSuccess', this.buildGameUI)
 
 }
 
@@ -17,17 +17,17 @@ UiUploader.prototype.showHint = function(message) {
   console.log(message)
 }
 
-UiUploader.prototype.enterGameUI = (roomInfo) => {
+UiUploader.prototype.buildGameUI = (res) => {
   let usernameBlock = document.getElementById('username-block')
   let app = document.getElementById('app')
   app.removeChild(usernameBlock)
-  console.log(roomInfo)
-  let gameBoard = document.createElement('div')
-  gameBoard.id = 'gameBoard'
-  let controller = document.createElement('div')
-  controller.id = 'controller'
-  app.appendChild(gameBoard)
-  app.appendChild(controller)
+  console.log(Object.keys(res.roomInfo.players))
+  let inner = '<div id="gameBoard">'
+  for (var i = 0; i < res.roomInfo.playerLimit; i++) {
+    inner += `<div class="playerBlock" id="${}"></div>`
+  }
+  inner += '</div><div id="playerBlock"></div>'
+  app.innerHTML = inner
 };
 
 export default new UiUploader()
