eecee7b51acd7e314e7e9c3b0b6e604775ab02ec
gundam1993 <gundam1993@gmail.com>
2017-07-26 11:08:14 +0800

    add delayEmitter in EE

--- app/lib/eventEmitter.js
+++ app/lib/eventEmitter.js
@@ -3,4 +3,11 @@
  */
 const EventEmitter = require('eventemitter3')
 const EE = new EventEmitter()
+
+EE.delayEmitter =  (name, time) => {
+  setTimeout(() => {
+      EE.emit(name)
+    }, time)
+}
+
 export default EE
