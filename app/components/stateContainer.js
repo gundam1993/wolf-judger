29ef76ee91e23a67b1110fbfc5b187caf49f48e6
gundam1993 <gundam1993@gmail.com>
2017-08-14 13:16:04 +0800

    add event handlr function in StateContainer

--- app/components/stateContainer.js
+++ app/components/stateContainer.js
@@ -54,6 +54,57 @@ class StateContainer extends React.Component {
       this.setState({roomInfo: res.roomInfo})
       this.setState({player: res.roomInfo.players[id]})
     })
+    EE.on('nextGamePhase', (res) => {
+      this.state.socket.emit('nextGamePhase')
+    })
+    EE.on('newGamePhase', (res) => {
+      this.setState({roomInfo: res.roomInfo})
+    })
+    EE.on('seerChosedPlayer', (res) => {
+      this.state.socket.emit('seerChosedPlayer', res)
+    })
+    EE.on('seerChosedDrop', (res) => {
+      this.state.socket.emit('seerChosedDrop', res)
+    })
+    EE.on('robberNotChange', () => {
+      this.state.socket.emit('robberNotChange')
+    })
+    EE.on('robberChosedPlayer', (res) => {
+      this.state.socket.emit('robberChosedPlayer', res)
+    })
+    EE.on('troubleMakerNotExchange', () => {
+      this.state.socket.emit('troubleMakerNotExchange')
+    })
+    EE.on('troubleMakerChosedPlayer', (res) => {
+      this.state.socket.emit('troubleMakerChosedPlayer', res)
+    })
+    EE.on('drunkChosedDrop', (res) => {
+      this.state.socket.emit('drunkChosedDrop', res)
+    })
+    EE.on('insomniacGetLastRole', () => {
+      this.state.socket.emit('insomniacGetLastRole')
+    })
+    EE.on('minionGetWerewolf', () => {
+      this.state.socket.emit('minionGetWerewolf')
+    })
+    EE.on('masonGetOtherMason', () => {
+      this.state.socket.emit('masonGetOtherMason')
+    })
+    EE.on('masonGotResult', () => {
+      this.state.socket.emit('masonGotResult')
+    })
+    EE.on('wereWolfGetOtherWereWolf', () => {
+      this.state.socket.emit('wereWolfGetOtherWereWolf')
+    })
+    EE.on('wereWolfChosedDrop', (res) => {
+      this.state.socket.emit('wereWolfChosedDrop', res)
+    })
+    EE.on('wereWolfGotResult', () => {
+      this.state.socket.emit('wereWolfGotResult')
+    })
+    EE.on('doppelgangerChosedPlayer', (res) => {
+      this.state.socket.emit('doppelgangerChosedPlayer', res)
+    })
   }
   render() {
     return (
