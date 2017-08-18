be960ee7a83af29e78ba6532d2d3e1f03dc99eaf
gundam1993 <gundam1993@gmail.com>
2017-08-18 17:19:11 +0800

    add actions updateRole & updateLastRole

--- app/actions/player.js
+++ app/actions/player.js
@@ -1,18 +1,26 @@
 export const UPDATE_USERNAME = 'UPDATE_USERNAME'
 export const UPDATE_USERID = 'UPDATE_USERID'
 export const PLAYER_READY = 'PLAYER_READY'
+export const UPDATE_ROLE = 'UPDATE_ROLE'
+export const UPDATE_LAST_ROLE = 'UPDATE_LAST_ROLE'
 
 //更新用户名
 export function updateUsername (username) {
   return {type: UPDATE_USERNAME, username: username}
 }
-
 //更新用户Id
 export function updateUserId (id) {
   return {type: UPDATE_USERID, id: id}
 }
-
 //用户准备
 export function playerReady() {
   return {type: PLAYER_READY }
+}
+// 更新用户初始身份
+export function updateRole(role) {
+  return {type: UPDATE_ROLE, role: role}
+}
+// 更新用户最终身份
+export function updateLastRole(role) {
+  return {type: UPDATE_LAST_ROLE, lastRole: role}
 }
