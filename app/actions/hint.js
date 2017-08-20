1c772b52fb5b73e653ad5d3d3425fe5ec82ed0ed
gundam1993 <gundam1993@gmail.com>
2017-08-20 16:38:11 +0800

    add actions hint

---
+++ app/actions/hint.js
@@ -0,0 +1,5 @@
+export const UPDATE_HINT_CONTEXT = 'UPDATE_HINT_CONTEXT'
+
+export function (context) {
+  return { type: UPDATE_HINT_CONTEXT, context: context}
+}
--- app/actions/index.js
+++ app/actions/index.js
@@ -5,7 +5,9 @@ import * as publics from './public'
 import * as player from './player'
 import * as join from './join'
 import * as room from './room'
+import * as hint from './hint'
 
-let action = Object.assign({}, publics, player, join, room)
+
+let action = Object.assign({}, publics, player, join, room, hint)
 
 export default action
