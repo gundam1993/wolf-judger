041205a5487ab0df80ed05304760ffe81f30bfa4
gundam1993 <gundam1993@gmail.com>
2017-08-20 17:21:20 +0800

    add action displayHint & hideHint

--- app/actions/hint.js
+++ app/actions/hint.js
@@ -1,5 +1,15 @@
 export const UPDATE_HINT_CONTEXT = 'UPDATE_HINT_CONTEXT'
+export const DISPLAY_HINT = 'DISPLAY_HINT'
+export const HIDE_HINT = 'HINT_HINT'
 
 export function updateHintContext (context) {
   return { type: UPDATE_HINT_CONTEXT, context: context}
+}
+
+export function displayHint() {
+  return { type: DISPLAY_HINT }
+}
+
+export function hideHint() {
+  return { type: HIDE_HINT }
 }
