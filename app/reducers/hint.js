5c51a8e12cd757565f753f6152abdd7a8e50f909
gundam1993 <gundam1993@gmail.com>
2017-08-20 17:22:52 +0800

    add displayHint & hideHint in hint reducer

--- app/reducers/hint.js
+++ app/reducers/hint.js
@@ -1,5 +1,6 @@
 let init = {
-  context: ''
+  context: '',
+  display: false,
 }
 
 const hint = (state = init, action) => {
@@ -7,6 +8,12 @@ const hint = (state = init, action) => {
   switch (action.type) {
     case 'UPDATE_HINT_CONTEXT' :
       newState = Object.assign({}, state, {context: action.context})
+      return newState
+    case 'DISPLAY_HINT' :
+      newState = Object.assign({}, state, {display: true})
+      return newState
+    case 'HIDE_HINT' :
+      newState = Object.assign({}, state, {display: false})
       return newState
     default :
       return state
