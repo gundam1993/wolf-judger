0c216eefd9b452384ca983a2bc766d96a95c903d
gundam1993 <gundam1993@gmail.com>
2017-08-20 16:50:29 +0800

    bug fix

--- app/actions/hint.js
+++ app/actions/hint.js
@@ -1,5 +1,5 @@
 export const UPDATE_HINT_CONTEXT = 'UPDATE_HINT_CONTEXT'
 
-export function (context) {
+export function updateHintContext (context) {
   return { type: UPDATE_HINT_CONTEXT, context: context}
 }
