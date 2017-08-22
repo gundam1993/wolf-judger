30498c56cc6ecb399245ac19fb9db3dc24baa70e
gundam1993 <gundam1993@gmail.com>
2017-08-22 13:51:31 +0800

    add case CHOOSE_DROP , REMOVE_DROP, CLEAN_DROP in drop reducer

--- app/reducers/drop.js
+++ app/reducers/drop.js
@@ -12,6 +12,24 @@ let init = {
       case 'HIDE_DROP' :
         newState = Object.assign({}, state, {display: false})
         return newState
+      case 'CHOOSE_DROP':
+        let chosenDrop = [].concat(state.chosenDrop)
+        if (chosenDrop.indexOf(action.index) === -1) {
+          chosenDrop.push(action.index)
+        }
+        newState = Object.assign({}, state, {chosenDrop: chosenDrop})
+        return newState
+      case 'REMOVE_DROP' :
+        let chosenDrop = [].concat(state.chosenDrop)
+        let dropIndex = chosenDrop.indexOf(action.index)
+        if (dropIndex !== -1) {
+          chosenDrop.splice(dropIndex, 1)
+        }
+        newState = Object.assign({}, state, {chosenDrop: chosenDrop})
+        return newState
+      case 'CLEAN_DROP' :
+        newState = Object.assign({}, state, {chosenDrop: []})
+        return newState
       default :
         return state
     }
