eb1719631047c19829456e4554cfc4fa306570e9
gundam1993 <gundam1993@gmail.com>
2017-08-28 17:42:59 +0800

    add inputType toggle button in bottomControlBar

--- app/components/bottomControlBar.js
+++ app/components/bottomControlBar.js
@@ -4,13 +4,21 @@ import React from 'react';
 
 class BottomControlBar extends React.Component {
   render() {
-    let readyButton
+    let readyButton, inputBox, audioButton, toggleIcon
     if (!this.props.ready) {
       readyButton = <div id="readyButton" onClick={this.props.onReadyClick}>准 备</div>
+    }
+    if (this.props.inputType === 'text') {
+      toggleIcon = <i className="iconfont icon-yuyin"></i>
+    } else {
+      toggleIcon = <i className="iconfont icon-duanluo"></i>
     }
     return (
       <div id="bottomControlBar">
         {readyButton}
+        <div id="inputTypeToggleButton">
+          {toggleIcon}
+        </div>
       </div>
     )
   }
@@ -18,12 +26,14 @@ class BottomControlBar extends React.Component {
 
 BottomControlBar.propTypes = {
   ready: React.PropTypes.bool.isRequired,
+  inputType: React.PropTypes.string.isRequired,
   onReadyClick: React.PropTypes.func.isRequired,
   onLeaveClick: React.PropTypes.func.isRequired,
 }
 
 BottomControlBar.defaultProps = {
   ready: true,
+  inputType: 'text',
 }
 
 export default BottomControlBar
