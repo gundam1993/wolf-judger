1b60f4433f24bd2f4b872324967dddff347f60fd
gundam1993 <gundam1993@gmail.com>
2017-08-30 17:56:04 +0800

    modify component hintInfo

--- app/components/hintInfo.js
+++ app/components/hintInfo.js
@@ -3,7 +3,16 @@ import React from 'react'
 
 class HintInfo extends React.Component {
   render() {
-
+    let from = <div className="hintFrom">{this.props.from}</div>
+    let content = <div className="hintContent">{this.props.content}</div>
+    if (this.props.from === 'system') {
+      from = ''
+      content = <div className="systemHint">{this.props.content}</div>
+    }
+    <div className="hintInfoContainer">
+      {from}
+      {content}
+    </div>
   }
 }
 
