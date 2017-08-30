5c77e7a08c1cfc9afadf9108e41d8a751abdf694
gundam1993 <gundam1993@gmail.com>
2017-08-30 16:27:46 +0800

    modify style

--- app/components/topControlBar.js
+++ app/components/topControlBar.js
@@ -6,7 +6,9 @@ class TopControlBar extends React.Component {
   render () {
     return (
       <div id="topControlBar">
-        <div id="exitRoomButtom" onClick={this.props.onLeaveClick}>退出</div>
+        <div id="exitRoomButtom" onClick={this.props.onLeaveClick}>
+          <i className="iconfont icon-fanhui"></i>
+        </div>
         <div id="roomInfo">
           <p>{this.props.roomName}</p>
           <p>{this.props.playerLimit}人局</p>
