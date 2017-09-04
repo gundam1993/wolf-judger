872324682da74e8e776596cedf1057e3db77ae53
gundam1993 <gundam1993@gmail.com>
2017-09-04 16:48:03 +0800

    remove subCOntent

--- app/containers/hintBar.js
+++ app/containers/hintBar.js
@@ -5,7 +5,6 @@ import hintBarComponent from '../components/hintBar'
 const mapStateToProps = (state) => {
   return {
     content: state.hint.content,
-    subContent: state.hint.subContent,
     display: state.hint.display,
   }
 }
