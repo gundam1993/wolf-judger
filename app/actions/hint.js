dac9fc222eae84cc86656d86de248a91fea0bbc9
gundam1993 <gundam1993@gmail.com>
2017-09-04 16:47:38 +0800

    remove old action of subContent & modify updateContent to addContent

--- app/actions/hint.js
+++ app/actions/hint.js
@@ -1,14 +1,9 @@
-export const UPDATE_HINT_CONTENT = 'UPDATE_HINT_CONTENT'
-export const UPDATE_SUB_CONTENT = 'UPDATE_SUB_CONTENT'
+export const ADD_HINT_CONTENT = 'ADD_HINT_CONTENT'
 export const DISPLAY_HINT = 'DISPLAY_HINT'
 export const HIDE_HINT = 'HINT_HINT'
 
-export function updateHintContent (content) {
-  return { type: UPDATE_HINT_CONTENT, content: content}
-}
-
-export function updateSubContent (content) {
-  return {type: UPDATE_SUB_CONTENT, content: content}
+export function addHintContent (content) {
+  return { type: ADD_HINT_CONTENT, content: content}
 }
 
 export function displayHint() {
