a1318163851d2aa28acce7ea579b1a9d88d61fc6
gundam1993 <gundam1993@gmail.com>
2017-09-20 17:22:10 +0800

    finish minion game flow

---
+++ app/gameFlow/minion.js
@@ -0,0 +1,17 @@
+import { phaseEnd } from './public'
+import * as FlowHelper from './flowHelper'
+
+// 爪牙发送消息确认行动开始
+export const minionStart = (store, actions) => {
+  if (store.getState().player.lastRole === 'minion') {
+    store.dispatch({type: 'MINION_GET_WEREWOLF'})
+  }
+}
+
+// 爪牙获得狼人信息,显示后结束阶段
+export const minionGetWerewolf = (store, actions, res) => {
+  console.log(res)
+  let info = res.wereWolf.length ? `目前身份是狼人的玩家有：${res.wereWolf.join('和')}。` : '目前场上没有狼人'
+  FlowHelper.judgeSay(store, actions, info)
+  FlowHelper.delayEmitter(phaseEnd, [store, actions, {phase: 'minion'}], 1000)
+}
--- roles/minion.js
+++ roles/minion.js
@@ -7,9 +7,9 @@ exports.getWerewolf = function (room, socket) {
   let wolf = []
   for (let key in room.players) {
     if (room.players[key].role === 'wereWolf') {
-      wolf.push(room.players[key])
+      wolf.push(room.players[key].username)
     }
   }
-  socket.emit('minionGetWerewolfResult', {wolf: wolf})
+  socket.emit('minionGetWerewolfResult', {wereWolf: wolf})
   socket.broadcast.in(room.name).emit('minionGetWerewolfResult')
 }
