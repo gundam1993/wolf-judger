8c092b0cbf7861e8e6449ad8d3a78f8949f635f7
gundam1993 <gundam1993@gmail.com>
2017-11-10 13:22:12 +0800

    finish seer flow

--- roles/seer.js
+++ roles/seer.js
@@ -2,19 +2,30 @@
  * 预言家流程处理模块
  */
 
-//预言家查看玩家身份
+// 预言家选择查看的对象
+exports.chooseTargetType = function (res, room, socket) {
+  console.log(res)
+  let target = res[0]
+  if (target) {
+    let type = target === "0" ? "player" : "drop"
+    socket.emit('seerChoseTargetType', { type: type })
+  }
+}
+// 预言家查看玩家身份
 exports.checkPlayerIdentity = function (player, room, socket) {
+  console.log(player)
   let roomName = room.name
-    player = room.players[player.id]
+    player = room.players[player[0]]
     if (player.username) {
-      socket.broadcast.in(roomName).emit('SeerChoosePlayerResult')
-      socket.emit('SeerChoosePlayerResult', { role: player.role })
+      socket.broadcast.in(roomName).emit('seerChoosePlayerResult')
+      socket.emit('seerChoosePlayerResult', { role: player.role })
     }
 }
 
 //预言家查看弃牌
 exports.checkDropIdentity = function (res, room, socket) {
-  let roles = [room.dropRole[res.dropRole[0]], room.dropRole[res.dropRole[1]]]
-  socket.broadcast.in(room.name).emit('SeerChooseDropResult')
-  socket.emit('SeerChooseDropResult', { roles: roles })
+  console.log(res)
+  let roles = [room.dropRole[res[0]], room.dropRole[res[1]]]
+  socket.broadcast.in(room.name).emit('seerChooseDropResult')
+  socket.emit('seerChooseDropResult', { roles: roles })
 }
